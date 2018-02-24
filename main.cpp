0825a4b32eaea297ec5eb7fb61658933a8f95425
TooKayAway <nhuff1@mail.csuchico.edu>
2018-02-23 19:28:27 -0800

    rough

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//author: Noah Huff
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +9,26 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-
-  return 0;
+        
+  for ( int i = 0; i != 1; i++ ) {
+                
+     cout<<"\n==GROCERY LIST MANAGER==";
+     cout<<"\nEnter your choice: ";
+     cout<<"\n (A)dd an item";
+     cout<<"\n (Q)uit";
+     cout<<"\nYour choice (A/Q): ";
+     cin>>input;
+     if ( input != 'q' ) {
+         i--;
+     }
+     if ( input == 'a' || input == 'A' ){
+       if ( numItems < 4 ) {
+          cout<<"\nWhat is the item?";
+          cin>>list[numItems];
+          numItems++;
+       } else { 
+          cout<<"\nYou'll need a bigger list!";                                                                                }
+     }
+  }   
+  return 0;                                                                                                   
 }
